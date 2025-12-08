dc222190538c4efea9c86b092b16f69fe6b4300c
SR0204 <k023g0023@g.neec.ac.jp>
2025-12-08 16:53:07 +0900

    スコアポップを作りたい

--- DirectXGame/ScorePop.h
+++ DirectXGame/ScorePop.h
@@ -1,22 +1,2 @@
 #pragma once
-#include "KamataEngine.h"
-#include <2d/Sprite.h>
-#include <array>
-#include <math/Vector2.h>
-
-class ScorePop {
-public:
-	ScorePop(const KamataEngine::Vector2& screenPos, int score, const std::array<uint32_t, 10>& textures);
-
-	void Update();
-	void Draw(ID3D12GraphicsCommandList*); // ← ここ重要
-	bool IsDead() const;
-
-private:
-	KamataEngine::Vector2 screenPos_;
-	float timer_;
-	float lifetime_;
-	float yOffset_;
-	int score_;
-	std::array<uint32_t, 10> textures_;
-};
+class ScorePop {};
