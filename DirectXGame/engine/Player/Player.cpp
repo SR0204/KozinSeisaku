169cb3b2cd024d7a179af45f69200f9702ce68e5
SR0204 <k023g0023@g.neec.ac.jp>
2025-12-22 01:12:03 +0900

    中ボスが出せない

--- DirectXGame/engine/Player/Player.cpp
+++ DirectXGame/engine/Player/Player.cpp
@@ -72,24 +72,6 @@ void Player::Update() {
 	dashParticles_->Update();
 	hipDropParticles_->Update();
 
-	// ダッシュ中の継続パーティクル
-	if ((Input::GetInstance()->PushKey(DIK_LSHIFT) || Input::GetInstance()->PushKey(DIK_RSHIFT)) && onGround_) {
-		Vector3 particlePos = worldTransform_.translation_ + Vector3(0, -kHeight / 2.0f, 0);
-		for (int i = 0; i < 2; ++i) { // フレームごとに少量
-			Vector3 vel = {(rand() % 100 - 50) / 200.0f, (rand() % 50) / 200.0f, 0};
-			dashParticles_->Emit(particlePos, vel, 0.5f);
-		}
-	}
-
-	// ヒップドロップ中の継続パーティクル
-	if (isHipDrop_) {
-		Vector3 particlePos = worldTransform_.translation_ + Vector3(0, -kHeight / 2.0f, 0);
-		for (int i = 0; i < 3; ++i) {
-			Vector3 vel = {(rand() % 100 - 50) / 100.0f, 0, (rand() % 100 - 50) / 100.0f};
-			hipDropParticles_->Emit(particlePos, vel, 0.5f);
-		}
-	}
-
 	// 回転制御
 	AnimateTurn();
 
@@ -164,14 +146,6 @@ void Player::InputMove() {
 	if (onGround_ && (input->TriggerKey(DIK_W) || input->TriggerKey(DIK_SPACE))) {
 		velocity_.y = jumpPower;
 		onGround_ = false;
-		// ダッシュ中ならパーティクル発生
-		if (isDash) {
-			Vector3 particlePos = worldTransform_.translation_ + Vector3(0, -kHeight / 2.0f, 0);
-			for (int i = 0; i < 10; ++i) {
-				Vector3 vel = {(rand() % 100 - 50) / 100.0f, (rand() % 50) / 100.0f, 0};
-				dashParticles_->Emit(particlePos, vel, 0.5f);
-			}
-		}
 	}
 
 	// ヒップドロップ発動
@@ -179,13 +153,6 @@ void Player::InputMove() {
 		if (input->TriggerKey(DIK_S) || input->TriggerKey(DIK_DOWN)) {
 			isHipDrop_ = true;
 			velocity_.y = std::min(velocity_.y, -0.2f); // 落下方向に少し勢いをつける程度
-
-			// ヒップドロップパーティクル発生
-			Vector3 particlePos = worldTransform_.translation_ + Vector3(0, -kHeight / 2.0f, 0);
-			for (int i = 0; i < 15; ++i) {
-				Vector3 vel = {(rand() % 100 - 50) / 50.0f, 0, (rand() % 100 - 50) / 50.0f};
-				hipDropParticles_->Emit(particlePos, vel, 0.5f);
-			}
 		}
 	}
 
